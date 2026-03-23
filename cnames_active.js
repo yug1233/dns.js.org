f78c6d7d5d429cd1550ba2edbc405f9430911049
yug1233 <yug50461@gmail.com>
2026-03-23 22:37:55 +0530

    Add mailgic to cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1096,6 +1096,7 @@ var cnames_active = {
   "magnet": "magnetjs.github.io/Magnet", // noCF? (don´t add this in a new PR)
   "mahdyar": "mahdyar.github.io/mahdyar.js.org",
   "mahmoud": "mahmoud-sagharjoughi.github.io/mahmoud",
+  "mailgic": "mailgic.netlify.app",
   "mailgo": "mailgo.netlify.com", // noCF
   "majestic": "moityjs.github.io/majestic",
   "maker": "microsoft.github.io/maker.js",
